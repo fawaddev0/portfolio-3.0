b7faa32ae1fbd7ba068d2f541fda92a121db2ad7
fawaddev0 <fawad.imran.000@gmail.com>
2026-02-24 16:16:31 +0500

    initial commit

---
+++ utils/Data/PersonalData.ts
@@ -0,0 +1,26 @@
+export const personalData = {
+  name: "Fawad Imran",
+  profile: "/pic3.jpg",
+  designation: "Full-Stack Web",
+  designationAlternateWords: [
+    "Developer.",
+    "Engineer.",
+    "Architect.",
+    "Innovator.",
+  ],
+  description:
+    "I’m Fawad Imran, a dedicated Full-Stack Developer specializing in TypeScript and crafting responsive, user‑centric interfaces with React and Next.js. I build full‑stack websites using Next.js and Prisma—leveraging Prisma’s type‑safe ORM to integrate robust database layers seamlessly into my applications. Passionate about continuous learning, I stay at the forefront of emerging technologies to deliver scalable, maintainable solutions that exceed expectations.",
+  email: "fawad.imran.000@gmail.com",
+  phone: "+923110253094",
+  address: "Karachi",
+  github: "https://github.com/fawaddev0",
+  facebook: "https://www.facebook.com/profile.php?id=100092595303195",
+  Instagram: "https://www.instagram.com/captain_ghost005",
+  linkedIn: "https://www.linkedin.com/in/fawadimran/",
+  twitter: "https://twitter.com/000_fawad",
+  stackOverflow: "https://stackoverflow.com/users/15349472/captain-ghost",
+  devUsername: "GHOST 005",
+  leetcode: "https://leetcode.com/u/rajaabdulbasit005/",
+  resume:
+    "/resume.pdf",
+};
