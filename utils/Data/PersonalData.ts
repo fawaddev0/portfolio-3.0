35087a62a1b5b050fd685943fae536ba4ef5f985
fawaddev0 <mrx7355608@gmail.com>
2026-03-01 07:09:38 +0500

    removed pwa support

--- utils/Data/PersonalData.ts
+++ utils/Data/PersonalData.ts
@@ -14,13 +14,8 @@ export const personalData = {
   phone: "+923110253094",
   address: "Karachi",
   github: "https://github.com/fawaddev0",
-  facebook: "https://www.facebook.com/profile.php?id=100092595303195",
-  Instagram: "https://www.instagram.com/captain_ghost005",
   linkedIn: "https://www.linkedin.com/in/fawadimran/",
   twitter: "https://twitter.com/000_fawad",
-  stackOverflow: "https://stackoverflow.com/users/15349472/captain-ghost",
-  devUsername: "GHOST 005",
-  leetcode: "https://leetcode.com/u/rajaabdulbasit005/",
-  resume:
-    "/resume.pdf",
+  devUsername: "fawaddev0",
+  resume: "/resume.pdf",
 };
